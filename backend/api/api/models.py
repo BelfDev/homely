beb47aa593a5f2f7feec585a7d067f2d40ded65a
Pedro Belfort <pedro.belfort@lutick.com>
2024-05-12 22:04:29 +0200

    Use modern user model syntax

--- backend/api/api/models.py
+++ backend/api/api/models.py
@@ -1,16 +1,19 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
 from api.extensions.db import db
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80), unique=False, nullable=False)
-    last_name = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(length=120), unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(80), unique=False, nullable=False)
+    last_name: Mapped[str] = mapped_column(String(80), unique=False, nullable=False)
 
     def to_json(self):
         return {
             "id": self.id,
+            "email": self.email,
             "firstName": self.first_name,
             "lastName": self.last_name,
-            "email": self.email
         }
