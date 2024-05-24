a451896a538109370ea339d61d9bdcafcda72df6
Pedro Belfort <pedro.belfort@lutick.com>
2024-05-24 10:40:36 +0200

    Config test fixtures

---
+++ backend/api/tests/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+
+from api import create_app
+from api.config import TestConfig
+from api.extensions import db as _db
+
+
+@pytest.fixture(scope='module')
+def app():
+    app = create_app(config_object=TestConfig)
+
+    with app.app_context():
+        _db.create_all()
+        yield app
+        _db.drop_all()
+
+
+@pytest.fixture(scope='module')
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture(scope='module')
+def db(app):
+    return _db
+
+
+@pytest.fixture(autouse=True)
+def clear_database(db):
+    db.session.remove()
+    db.drop_all()
+    db.create_all()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
