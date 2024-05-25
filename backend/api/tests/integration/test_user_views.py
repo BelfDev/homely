d41f810c8b7cf391d87741ae49e6e1dc95b115e6
Pedro Belfort <pedro.belfort@lutick.com>
2024-05-25 21:40:08 +0200

    Refine helper test functions

--- backend/api/tests/integration/test_user_views.py
+++ backend/api/tests/integration/test_user_views.py
@@ -1,22 +1,22 @@
 from api.user.models import User
-from .user_aux import login_route, create_user, login_user, get_user_by_email
+from .user_aux import login_route, client_create_user, client_login_user, db_get_user_by_email, db_add_user
 
 
 def test_register_user_success(client, app):
     valid_email = "test@test.com"
 
     # Check if user is not in the database
-    assert get_user_by_email(app, valid_email) is None
+    assert db_get_user_by_email(app, valid_email) is None
 
     # Given valid user data, when registering a new user, then the user is created
-    response = create_user(client, valid_email, "test123", "Test", "User")
+    response = client_create_user(client, valid_email, "test123", "Test", "User")
     assert response.status_code == 201
     data = response.get_json()
     assert data['email'] == valid_email
     assert 'id' in data
 
     # Check if user is in the database
-    user = get_user_by_email(app, valid_email)
+    user = db_get_user_by_email(app, valid_email)
     assert user is not None
     assert user.email == valid_email
 
@@ -25,10 +25,10 @@ def test_register_user_existing_email(client, app):
     valid_email = "test@test.com"
 
     # Create a user
-    create_user(client, valid_email, "test123", "Test", "User")
+    client_create_user(client, valid_email, "test123", "Test", "User")
 
     # Given the user exists, when registering the same user, then the user is not created and an error is returned
-    response = create_user(client, valid_email, "test123", "Test", "User")
+    response = client_create_user(client, valid_email, "test123", "Test", "User")
     assert response.status_code == 400
     data = response.get_json()
     assert data['msg'] == "Email already exists"
@@ -36,24 +36,23 @@ def test_register_user_existing_email(client, app):
 
 def test_register_user_missing_fields(client):
     # Given missing fields, when registering a new user, then an error is returned
-    response = create_user(client, "test2@test.com", "test123", "", "")
+    response = client_create_user(client, "test2@test.com", "test123", "", "")
     assert response.status_code == 400
 
 
 def test_register_user_invalid_email(client):
     # Given invalid email, when registering a new user, then an error is returned
-    response = create_user(client, "not-an-email", "test123", "Test", "User")
+    response = client_create_user(client, "not-an-email", "test123", "Test", "User")
     assert response.status_code == 400
 
 
 def test_login_success(client, db):
     # Create a test user
     user = User(email="test@test.com", password="test123", first_name="Test", last_name="User")
-    db.session.add(user)
-    db.session.commit()
+    db_add_user(db, user)
 
     # Attempt to login with correct credentials
-    response = login_user(client, "test@test.com", "test123")
+    response = client_login_user(client, "test@test.com", "test123")
     assert response.status_code == 200
     data = response.get_json()
     assert 'access_token' in data
@@ -76,11 +75,10 @@ def test_login_invalid_json(client):
 def test_login_incorrect_email(client, db):
     # Create a test user
     user = User(email="test@test.com", password="test123", first_name="Test", last_name="User")
-    db.session.add(user)
-    db.session.commit()
+    db_add_user(db, user)
 
     # Attempt to login with incorrect email
-    response = login_user(client, "wrong@test.com", "test123")
+    response = client_login_user(client, "wrong@test.com", "test123")
     assert response.status_code == 401
     data = response.get_json()
     assert data['msg'] == "Bad email or password"
@@ -89,11 +87,10 @@ def test_login_incorrect_email(client, db):
 def test_login_incorrect_password(client, db):
     # Create a test user
     user = User(email="test2@test.com", password="test123", first_name="Test", last_name="User")
-    db.session.add(user)
-    db.session.commit()
+    db_add_user(db, user)
 
     # Attempt to login with incorrect password
-    response = login_user(client, "test2@test.com", "wrongpassword")
+    response = client_login_user(client, "test2@test.com", "wrongpassword")
     assert response.status_code == 401
     data = response.get_json()
     assert data['msg'] == "Bad email or password"
--- backend/api/tests/integration/user_aux.py
+++ backend/api/tests/integration/user_aux.py
@@ -5,7 +5,7 @@ login_route = '/api/v1/users/login'
 
 
 # Helper functions
-def create_user(client, email, password, first_name, last_name):
+def client_create_user(client, email, password, first_name, last_name):
     return client.post(register_route, json={
         "email": email,
         "password": password,
@@ -14,13 +14,19 @@ def create_user(client, email, password, first_name, last_name):
     })
 
 
-def login_user(client, email, password):
+def client_login_user(client, email, password):
     return client.post(login_route, json={
         "email": email,
         "password": password
     })
 
 
-def get_user_by_email(app, email):
+def db_get_user_by_email(app, email):
     with app.app_context():
         return User.query.filter_by(email=email).first()
+
+
+def db_add_user(db, user: User):
+    db.session.add(user)
+    db.session.commit()
+    return user
