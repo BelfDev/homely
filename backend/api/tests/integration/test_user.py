22f07b3fb6ee50ba50f04893f0cbecae4ef197ad
Pedro Belfort <pedro.belfort@lutick.com>
2024-05-25 20:10:39 +0200

    Create user registration integration tests

--- backend/api/tests/integration/test_user.py
+++ backend/api/tests/integration/test_user.py
@@ -1,15 +1,20 @@
-import json
 from api.user.models import User
 
 
-def test_register_user(client, db):
+def test_register_user(client, app):
     endpoint = '/api/v1/users'
+    valid_email = "test@test.com"
+
+    # Check if user is not in the database
+    with app.app_context():
+        user = User.query.filter_by(email=valid_email).first()
+        assert user is None
 
     # Given valid user data,
     # when registering a new user,
     # then the user is created.
     response = client.post(endpoint, json={
-        "email": "test@test.com",
+        "email": valid_email,
         "password": "test123",
         "firstName": "Test",
         "lastName": "User"
@@ -19,11 +24,17 @@ def test_register_user(client, db):
     assert data['email'] == "test@test.com"
     assert 'id' in data
 
+    # Check if user is in the database
+    with app.app_context():
+        user = User.query.filter_by(email="test@test.com").first()
+        assert user is not None
+        assert user.email == valid_email
+
     # Given the user exists,
     # when registering the same user,
     # then the user is not created and an error is returned.
     response = client.post(endpoint, json={
-        "email": "test@test.com",
+        "email": valid_email,
         "password": "test123",
         "firstName": "Test",
         "lastName": "User"
