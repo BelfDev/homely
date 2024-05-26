b7dd7854a7ff3225e9995b308577928ffe7c7acc
Pedro Belfort <pedro.belfort@lutick.com>
2024-05-26 21:46:52 +0200

    Init iOS App

---
+++ frontend/homely/homely/homelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  homelyApp.swift
+//  homely
+//
+//  Created by Pedro Belfort on 26.05.24.
+//
+
+import SwiftUI
+
+@main
+struct homelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
