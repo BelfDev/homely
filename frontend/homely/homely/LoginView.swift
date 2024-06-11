c139dfa9d11728915229a88454abe4858dd72dcc
Pedro Belfort <pedro.belfort@lutick.com>
2024-06-11 10:58:54 +0200

    Extract subviews

--- frontend/homely/homely/LoginView.swift
+++ frontend/homely/homely/LoginView.swift
@@ -9,9 +9,6 @@ import SwiftUI
 
 struct LoginView: View {
     
-    @State private var email: String = ""
-    @State private var password: String = ""
-    
     var body: some View {
         
         GeometryReader { geometry in
@@ -31,8 +28,7 @@ struct LoginView: View {
                             .foregroundStyle(.white)
                             .font(.largeTitle)
                             .frame(maxWidth: .infinity, minHeight: geometry.size.height * 0.3)
-                        
-                        
+
                         VStack {
                             Spacer()
                                 .frame(height: 32.0)
@@ -44,87 +40,33 @@ struct LoginView: View {
                             Spacer()
                                 .frame(height: 32.0)
                             
-                            VStack(
-                                alignment: .leading,
-                                spacing: 8.0) {
-                                    Text("Login")
-                                        .font(.system(size: 16, weight: .medium))
-                                        .foregroundColor(.secondary)
-                                        .frame(alignment: .leading)
-                                    
-                                    TextField("", text: $email)
-                                        .font(.system(size: 17, weight: .thin))
-                                        .foregroundColor(.primary)
-                                        .frame(height: 48.0)
-                                        .padding(.horizontal, 12)
-                                        .background(.gray)
-                                        .cornerRadius(16.0)
-                                }
+                            EmailInputField()
                             
                             Spacer()
                                 .frame(height: 24.0)
                             
-                            VStack(
-                                alignment: .leading,
-                                spacing: 8.0) {
-                                    Text("Password")
-                                        .font(.system(size: 16, weight: .medium))
-                                        .foregroundColor(.secondary)
-                                        .frame(height: 15, alignment: .leading)
-                                    
-                                    SecureField("", text: $password)
-                                        .font(.system(size: 17, weight: .thin))
-                                        .foregroundColor(.primary)
-                                        .frame(height: 48.0)
-                                        .padding(.horizontal, 12)
-                                        .background(.gray)
-                                        .cornerRadius(16.0)
-                                }
+                            PasswordInputField()
                             
                             Spacer()
                                 .frame(height: 8.0)
                             
                             // OK
-                            HStack {
-                                Spacer()
-                                Button("Forgot password?") {
-                                    print("Forgot")
-                                }
-                            }
+                            ForgotPasswordButton()
                             
                             
                             Spacer()
                             
                             // OK
-                            Button {
-                                print("Log user in")
-                            } label: {
-                                Text("Login")
-                                    .font(.title2)
-                                    .bold()
-                                    .foregroundColor(.white)
-                            }
-                            .frame(maxWidth: .infinity, minHeight: 56.0)
-                            .background(.black)
-                            .cornerRadius(8.0)
+                            LoginButton()
                             
                             Spacer()
                                 .frame(height: 8.0)
                             
                             // OK
-                            HStack {
-                                Text("Don't have an account?")
-                                Spacer()
-                                Button {
-                                    print("Sign up")
-                                } label: {
-                                    HStack {
-                                        Text("Sign Up")
-                                        Image(systemName: "arrow.right")
-                                    }
-                                }
-                                
-                            }
+                            SignUpRow()
+                            
+//                            Spacer()
+//                                .frame(height: 600.0)
                             
                             
                         }
@@ -136,6 +78,7 @@ struct LoginView: View {
                             )
                             .foregroundStyle(.white)
                         )
+                    
                     }
                     .frame(maxWidth: .infinity, minHeight: geometry.size.height)
                 }
@@ -149,4 +92,95 @@ struct LoginView: View {
 
 #Preview {
     LoginView()
+}
+
+struct LoginButton: View {
+    var body: some View {
+        Button {
+            print("Log user in")
+        } label: {
+            Text("Login")
+                .font(.title2)
+                .bold()
+                .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, minHeight: 56.0)
+        .background(.black)
+        .cornerRadius(8.0)
+    }
+}
+
+struct SignUpRow: View {
+    var body: some View {
+        HStack {
+            Text("Don't have an account?")
+            Spacer()
+            Button {
+                print("Sign up")
+            } label: {
+                HStack {
+                    Text("Sign Up")
+                    Image(systemName: "arrow.right")
+                }
+            }
+            
+        }
+    }
+}
+
+struct ForgotPasswordButton: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            Button("Forgot password?") {
+                print("Forgot")
+            }
+        }
+    }
+}
+
+struct PasswordInputField: View {
+    @State private var password: String = ""
+    
+    var body: some View {
+        VStack(
+            alignment: .leading,
+            spacing: 8.0) {
+                Text("Password")
+                    .font(.system(size: 16, weight: .medium))
+                    .foregroundColor(.secondary)
+                    .frame(height: 15, alignment: .leading)
+                
+                SecureField("", text: $password)
+                    .font(.system(size: 17, weight: .thin))
+                    .foregroundColor(.primary)
+                    .frame(height: 48.0)
+                    .padding(.horizontal, 12)
+                    .background(.gray)
+                    .cornerRadius(16.0)
+            }
+    }
+}
+
+struct EmailInputField: View {
+    @State private var email: String = ""
+    
+    var body: some View {
+        VStack(
+            alignment: .leading,
+            spacing: 8.0) {
+                Text("Login")
+                    .font(.system(size: 16, weight: .medium))
+                    .foregroundColor(.secondary)
+                    .frame(alignment: .leading)
+                
+                TextField("", text: $email)
+                    .font(.system(size: 17, weight: .thin))
+                    .foregroundColor(.primary)
+                    .frame(height: 48.0)
+                    .padding(.horizontal, 12)
+                    .background(.gray)
+                    .cornerRadius(16.0)
+            }
+    }
 }
