9a68818d153764417cde6578b41057e7ff47e157
Pedro Belfort <pedro.belfort@lutick.com>
2024-10-13 13:07:59 +0200

    Refine header layout

--- frontend/homely/Homely/Features/Home/HomeScreen.swift
+++ frontend/homely/Homely/Features/Home/HomeScreen.swift
@@ -10,37 +10,33 @@ import SwiftUI
 struct HomeScreen: View {
     
     @ThemeProvider private var theme
-    
-    let columns = [
-        GridItem(.flexible(), spacing: 16),
-        GridItem(.flexible(), spacing: 16)
-    ]
     @State private var vm: HomeViewModel
     
     init(_ components: ComponentManager) {
         vm = HomeViewModel(with: components)
     }
     
+    private let columns = [
+        GridItem(.flexible(), spacing: 16),
+        GridItem(.flexible(), spacing: 16)
+    ]
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 8) {
-                Text("Morning, \nSofie!")
-                    .font(theme.font.h3)
-                
+                HStack(alignment: .firstTextBaseline) {
+                    Text(vm.greeting)
+                        .font(theme.font.h3)
+                    Spacer()
+                    Button("Logout", action: vm.logout)
+                        .buttonStyle(.bordered)
+                }
                 Text(vm.currentDate)
                     .font(theme.font.h6)
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding(.vertical, 16)
-             
-            Spacer(minLength: 32)
-            
-            Button {
-                vm.logout()
-            } label: {
-                Text("Logout")
-            }
-            
+                        
             Spacer(minLength: 40)
             
             LazyVGrid(columns: columns, spacing: 16) {
@@ -52,7 +48,7 @@ struct HomeScreen: View {
             .padding(8)
         }
         .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(.horizontal, 24)
+        .padding(.horizontal, 16)
         .background(theme.color.surface)
         .scrollBounceBehavior(.basedOnSize)
         .toolbarTitleDisplayMode(.inlineLarge)
--- frontend/homely/Homely/Features/Home/HomeViewModel.swift
+++ frontend/homely/Homely/Features/Home/HomeViewModel.swift
@@ -12,12 +12,14 @@ final class HomeViewModel {
     private let homelyClient: HomelyAPIClient
     private let session: SessionManager
     
+    var greeting: String = ""
     var currentDate: String = ""
     
     init(with components: ComponentManager) {
         self.homelyClient = components.homelyClient
         self.session = components.session
         self.currentDate = getCurrentFormattedDate()
+        self.greeting = getGreetingMessage()
     }
     
     func logout() {
@@ -25,8 +27,13 @@ final class HomeViewModel {
     }
     
     private func getCurrentFormattedDate() -> String {
-          let formatter = DateFormatter()
-          formatter.dateFormat = "EEEE, d MMMM" // "Thursday, 13 October" format
-          return formatter.string(from: Date())
-      }
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE, d MMMM" // "Thursday, 13 October" format
+        return formatter.string(from: Date())
+    }
+    
+    private func getGreetingMessage() -> String {
+        return "Morning, \nSofie!"
+    }
+    
 }
