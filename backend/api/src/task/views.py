8dd3034b62459e996d0519435cfbdda0d2bfdd8a
Pedro Belfort <pedro.belfort@lutick.com>
2024-11-02 22:55:36 +0100

    Add updat task and retrieve all by user id

--- backend/api/src/task/views.py
+++ backend/api/src/task/views.py
@@ -33,12 +33,45 @@ def create_task():
 
     task_from_db = Task.query.get(new_task.id)
 
-    result = task_wire_out_schema.dump(task_from_db)
-    return result, 201
+    return task_wire_out_schema.dump(task_from_db), 201
+
+
+@bp.route("/v1/tasks/<uuid:task_id>", methods=["PUT"])
+@jwt_required()
+def update_task(task_id):
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    task = Task.query.get_or_404(task_id)
+    if not task.created_by == current_user.id:
+        return jsonify({"msg": "Task not found or access is forbidden"}), 404
+
+    data = request.json
+    try:
+        updated_task = task_wire_in_schema.load(data, instance=task)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+    for key, value in updated_task.items():
+        setattr(task, key, value)
+
+    db.session.commit()
+
+    return task_wire_out_schema.dump(updated_task), 200
 
 
 @bp.route("/v1/tasks/<uuid:task_id>", methods=["GET"])
 @jwt_required()
 def get_task(task_id):
     task = Task.query.get_or_404(task_id)
+    if not task.created_by == current_user.id:
+        return jsonify({"msg": "Task not found or access is forbidden"}), 404
+
     return task_wire_out_schema.dump(task), 200
+
+
+@bp.route("/v1/tasks", methods=["GET"])
+@jwt_required()
+def get_all_tasks():
+    user_tasks = Task.query.filter_by(created_by=current_user.id).all()
+    return task_wire_out_schema.dump(user_tasks, many=True), 200
