720af77a73d5135c95ef14cab4a65d0710bcdf63
Pedro Belfort <belfortdev@gmail.com>
2025-01-19 20:56:50 +0100

    Create new task form validations

---
+++ frontend/homely/Homely/Features/Tasks/NewTask/NewTaskFormValidations.swift
@@ -0,0 +1,8 @@
+//
+//  NewTaskFormValidations.swift
+//  Homely
+//
+//  Created by Pedro Belfort on 19.01.25.
+//
+
+import Foundation
---
+++ frontend/homely/Homely/Wire/Output/NewTaskRequestBody.swift
@@ -0,0 +1,8 @@
+//
+//  NewTaskRequestBody.swift
+//  Homely
+//
+//  Created by Pedro Belfort on 19.01.25.
+//
+
+import Foundation
